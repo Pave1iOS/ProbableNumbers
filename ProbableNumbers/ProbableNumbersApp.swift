cf94db5dc37ce3b5bf4e5fd3198c33ae319208d0
Pave1iOS <PGribachev94@yandex.ru>
2024-03-12 14:09:10 +0300

    Initial Commit

---
+++ ProbableNumbers/ProbableNumbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProbableNumbersApp.swift
+//  ProbableNumbers
+//
+//  Created by Pavel Gribachev on 12.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ProbableNumbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
